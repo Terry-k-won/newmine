2a678aa51547120f6eb9980b2b2a8442da4c3f2e
Terry-k-won <hiexpectations90@gmail.com>
2025-10-09 10:23:22 +0000

    클릭이벤트 추가

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.querySelector('.imagetext-container');
-    const imageElement = container.querySelector('.image-element');
-
-    imageElement.addEventListener('click', function() {
-        // imagetext-container에 'active' 클래스를 토글하여 CSS 변경을 적용
-        container.classList.toggle('active');
-    });
+document.querySelectorAll('.imagetext-container').forEach(container => {
+  container.addEventListener('click', function() {
+    container.classList.toggle('active');
+  });
 });
