a614003dbed620a67e4091d667cb4f72b11f0ef2
Terry-k-won <hiexpectations90@gmail.com>
2025-10-10 23:34:37 +0000

    toggle로 add 대체

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ document.querySelectorAll('.imagetext-container').forEach(container => {
   container.addEventListener('click', function() {
     container.classList.toggle('active');
     document.querySelectorAll('.intro-ment').forEach(function(el) {
-      el.classList.add('activeIntro');
+      el.classList.toggle('activeIntro');
     });
     document.querySelectorAll('.highlight').forEach(function(el) {
-      el.classList.add('activeHighlight');
+      el.classList.toggle('activeHighlight');
     });
   });
 });
