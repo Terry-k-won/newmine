b715c3e109af4f1bb01a87191c234a345dec6a62
Terry-k-won <hiexpectations90@gmail.com>
2025-10-10 23:23:41 +0000

    textbox에도 효과 적용되게

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.querySelectorAll('.imagetext-container').forEach(container => {
   container.addEventListener('click', function() {
     container.classList.toggle('active');
+    document.querySelector('.intro-ment').classList.toggle('activeEffect');
+    document.querySelector('.highlight').classList.toggle('activeHighlight');
   });
 });
