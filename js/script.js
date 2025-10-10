6b2acf6d69327cb7ca340cbad0499fdc25c36d78
Terry-k-won <hiexpectations90@gmail.com>
2025-10-10 23:32:22 +0000

    transition 적용

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 document.querySelectorAll('.imagetext-container').forEach(container => {
   container.addEventListener('click', function() {
     container.classList.toggle('active');
-    document.querySelector('.intro-ment').classList.toggle('activeEffect');
-    document.querySelector('.highlight').classList.toggle('activeHighlight');
+    document.querySelectorAll('.intro-ment').forEach(function(el) {
+      el.classList.add('activeIntro');
+    });
+    document.querySelectorAll('.highlight').forEach(function(el) {
+      el.classList.add('activeHighlight');
+    });
   });
 });
